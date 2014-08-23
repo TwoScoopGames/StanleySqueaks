74686bdd3ce00a4d7687be0ff49a88d1ceb8598c
waiyinglam <code@waiyinglam.com>
2014-08-23 01:04:08 -0400

    added floor

--- game.js
+++ game.js
@@ -25,17 +25,39 @@ function centerText(context, text, offsetX, offsetY) {
 	context.fillText(text, x, y);
 }
 
+var drawBlock = function(context) {
+	context.strokeStyle = "blue";
+	context.strokeRect(this.x, this.y, this.width, this.height);
+};
 
 //this refers to current scene
 game.scenes.add("title", new Splat.Scene(canvas, function() {
 	// initialization
-	this.player = new Splat.Entity(100, 100, 100, 100);
+	var blockSize = 20;
+	this.blocks = [];
+
+	for(var x=0; x<canvas.width; x+=blockSize) {
+		var block = new Splat.Entity(x, canvas.height-blockSize, blockSize, blockSize);
+		block.draw = drawBlock;
+		this.blocks.push(block);
+	}
+
+	this.player = new Splat.Entity(100, 100, 40, 50); //hamster jones
 	this.player.draw = function(context) {
 		context.fillStyle = "red";
 		context.fillRect(this.x, this.y, this.width, this.height);
 	};
 }, function(elapsedMillis) {
 	// simulation
+	var movement = 0.3;
+
+	if(game.keyboard.isPressed("right")) {
+		this.player.vx = movement; //how fast he moves
+	} else if(game.keyboard.isPressed("left")) {
+		this.player.vx = -movement; //how fast he moves
+	} else {
+		this.player.vx = 0;
+	}
 	this.player.move(elapsedMillis);
 }, function(context) {
 	// draw
@@ -46,6 +68,10 @@ game.scenes.add("title", new Splat.Scene(canvas, function() {
 	context.font = "25px helvetica";
 	centerText(context, "Blank SplatJS Project", 0, canvas.height / 2 - 13);
 
+	for(var i=0; i<this.blocks.length; i++) {
+		this.blocks[i].draw(context);
+	}
+	
 	this.player.draw(context);
 }));
 
