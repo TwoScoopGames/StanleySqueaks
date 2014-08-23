1df0221ea5c1489d96dff4a0711aaa6a7e90e287
Eric Lathrop <eric@ericlathrop.com>
2014-08-23 18:52:32 -0400

    Better first level.

--- levels.js
+++ levels.js
@@ -3,98 +3,139 @@ module.exports = [
 		"name": "Level 1",
 		"objects": [
 			{
-				"type": "spawn",
-				"x": 500,
-				"y": 100,
-			},
-			{
-				"type": "goal",
-				"x": 100,
-				"y": 400,
-			},
-			//platform
-			{
-				"type": "block",
-				"x": 500,
-				"y": 500,
-			},
-			{
-				"type": "block",
-				"x": 532,
-				"y": 500,
-			},
-			{
-				"type": "block",
-				"x": 564,
-				"y": 500,
-			},
-			{
-				"type": "block",
-				"x": 596,
-				"y": 500,
-			},
-			//platform 2
-			{
-				"type": "block",
-				"x": 800,
-				"y": 436,
-			},
-			{
-				"type": "block",
-				"x": 832,
-				"y": 436,
-			},
-			{
-				"type": "block",
-				"x": 864,
-				"y": 436,
-			},
-			{
-				"type": "block",
-				"x": 896,
-				"y": 436,
-			},
-			//platform 3
-			{
-				"type": "block",
-				"x": 1007,
-				"y": 372,
-			},
-			{
-				"type": "block",
-				"x": 1039,
-				"y": 372,
-			},
-			//floor
-			{
 				"type": "block",
 				"x": 32,
-				"y": 608,
+				"y": 576
 			},
 			{
 				"type": "block",
 				"x": 64,
-				"y": 608,
+				"y": 576
 			},
 			{
 				"type": "block",
 				"x": 96,
-				"y": 608,
+				"y": 576
+			},
+			{
+				"type": "block",
+				"x": 0,
+				"y": 544
 			},
 			{
 				"type": "block",
 				"x": 128,
-				"y": 608,
+				"y": 576
 			},
 			{
 				"type": "block",
 				"x": 160,
-				"y": 608,
+				"y": 576
 			},
 			{
 				"type": "block",
 				"x": 192,
-				"y": 608,
+				"y": 576
+			},
+			{
+				"type": "block",
+				"x": 224,
+				"y": 576
+			},
+			{
+				"type": "block",
+				"x": 256,
+				"y": 576
+			},
+			{
+				"type": "block",
+				"x": 448,
+				"y": 576
+			},
+			{
+				"type": "block",
+				"x": 480,
+				"y": 576
+			},
+			{
+				"type": "block",
+				"x": 416,
+				"y": 576
+			},
+			{
+				"type": "block",
+				"x": 512,
+				"y": 576
+			},
+			{
+				"type": "block",
+				"x": 544,
+				"y": 576
+			},
+			{
+				"type": "block",
+				"x": 576,
+				"y": 576
+			},
+			{
+				"type": "block",
+				"x": 608,
+				"y": 576
+			},
+			{
+				"type": "block",
+				"x": 768,
+				"y": 576
+			},
+			{
+				"type": "block",
+				"x": 800,
+				"y": 576
+			},
+			{
+				"type": "block",
+				"x": 832,
+				"y": 576
+			},
+			{
+				"type": "block",
+				"x": 864,
+				"y": 576
+			},
+			{
+				"type": "block",
+				"x": 1088,
+				"y": 544
+			},
+			{
+				"type": "block",
+				"x": 1056,
+				"y": 576
+			},
+			{
+				"type": "block",
+				"x": 1024,
+				"y": 576
+			},
+			{
+				"type": "block",
+				"x": 992,
+				"y": 576
+			},
+			{
+				"type": "block",
+				"x": 960,
+				"y": 576
+			},
+			{
+				"type": "spawn",
+				"x": 79,
+				"y": 348
+			},
+			{
+				"type": "goal",
+				"x": 972,
+				"y": 464
 			}
 		]
 	}
