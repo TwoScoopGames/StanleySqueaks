8548c08e338c9bd586abdd7b9a31b883992a6c5d
Alex Bezuska <abezuska@gmail.com>
2015-04-08 20:19:23 -0400

    Better start script

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
+SCRIPTPATH='~/games/stanleysqueaks'
 killall xjoy2key
-/home/kiosk-mode/xjoy2key/src/xjoy2key xjoy2key.cfg &
-firefox -p "Full Screen" -no-remote index.html
+~/xjoy2key/src/xjoy2key "$SCRIPTPATH/xjoy2key.cfg" &
+firefox -p "Full Screen" -no-remote "$SCRIPTPATH/index.html"
 killall xjoy2key
