4a5e1266d8a281e0d28c17afe04fdcc8797d0ffa
Alex Bezuska <abezuska@gmail.com>
2015-04-08 20:31:47 -0400

    Fix paths

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-SCRIPTPATH='~/games/stanleysqueaks'
+SCRIPTPATH='/home/kiosk-mode/games/stanleysqueaks'
 killall xjoy2key
-~/xjoy2key/src/xjoy2key "$SCRIPTPATH/xjoy2key.cfg" &
+/home/kiosk-mode/xjoy2key/src/xjoy2key "$SCRIPTPATH/xjoy2key.cfg" &
 firefox -p "Full Screen" -no-remote "$SCRIPTPATH/index.html"
 killall xjoy2key
